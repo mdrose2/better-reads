220be321c39046df42c070ac077d50c4f06d017e
mdrose2 <mariahdawnrose@hotmail.com>
2026-03-04 09:51:16 -0800

    Added whitenoise for loading static files on Render

--- book_review_site/settings.py
+++ book_review_site/settings.py
@@ -56,6 +56,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -127,12 +128,16 @@ USE_TZ = True
 # STATIC FILES
 # ==============================================================================
 
-STATIC_URL = 'static/'
+# ==============================================================================
+# STATIC FILES
+# ==============================================================================
+
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
-
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # ==============================================================================
 # CUSTOM USER MODEL
