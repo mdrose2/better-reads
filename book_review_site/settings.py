43f8d2c8a50ce7294aca36494d197ca6737744a5
mdrose2 <mariahdawnrose@hotmail.com>
2026-03-07 11:11:37 -0800

    Setting debug back to false for production.

--- book_review_site/settings.py
+++ book_review_site/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # ==============================================================================
 
 SECRET_KEY = 'django-insecure-ay*%6nuktf1gpt#6%q7n1)nc(s9s$7a7!m1r+%z7avb(%z$b$1'
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = [
     'better-reads-zlcy.onrender.com', 
     '127.0.0.1', 
